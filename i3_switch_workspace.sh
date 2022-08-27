56eb80a9c316e1703f6951f0e66f95e36d0af2ce
“Eduardo <efeoflus@andrew.cmu.edu>
2022-08-27 12:24:41 +0300

    new tweaks

--- i3_switch_workspace.sh
+++ i3_switch_workspace.sh
@@ -13,5 +13,15 @@ then
     i3_empty_workspace.sh
 elif [ -n "${WORKSPACE}" ]
 then
-    i3-msg workspace "${WORKSPACE}"
+    # https://www.reddit.com/r/i3wm/comments/wgcpae/help_with_keybind_for_moving_workspace_to/
+    message=$(i3-msg -t get_workspaces)
+    output=$(echo "${message}" | jq '.[] | select(.focused==true).output')
+    curr_output=$(echo "${message}" | jq ".[] | select(.name==\"${WORKSPACE}\").output")
+    if [ -n "${curr_output}" ] && [ "${output}" != "${curr_output}" ]
+    then
+	i3-msg "workspace \"${WORKSPACE}\"; move workspace to output ${output}"
+    else
+	i3-msg "workspace \"${WORKSPACE}\""
+    fi
+    #i3-msg workspace "${WORKSPACE}"
 fi
